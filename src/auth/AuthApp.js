643b2e7f3899da627ecab5dcb6b4bbe00ba43da9
Vasily93 <markovavasily@gmail.com>
2019-06-10 12:03:35 -0700

    set data to save user info to firebase in our AuthApp and began appending our header in AuthApp as well

--- src/auth/AuthApp.js
+++ src/auth/AuthApp.js
@@ -1,5 +1,6 @@
 import Component from '../Component.js';
-import { auth } from '../services/firebase.js';
+import Header from '../shared/Header.js';
+import { auth, usersRef } from '../services/firebase.js';
 
 const ui = new firebaseui.auth.AuthUI(auth);
 
@@ -7,14 +8,28 @@ class AuthApp extends Component {
     render() {
         const dom = this.renderDOM();
 
+        const header = new Header();
+
+
         ui.start('#firebaseui-auth-container', {
             signInOptions: [
                 firebase.auth.EmailAuthProvider.PROVIDER_ID,
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID
             ],
             signInSuccessUrl: './',
-            credentialHelper: firebaseui.auth.CredentialHelper.NONE
-            
+            credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+            callbacks: {
+                signInSuccessWithAuthResult(authResult) {
+                    const user = authResult.user;
+                    usersRef.child(user.uid)
+                        .set({
+                            uid: user.uid,
+                            displayName: user.displayName,
+                            photoURL: user.photoURL
+                        });
+                    return true;
+                }
+            }
         });
 
         return dom;
