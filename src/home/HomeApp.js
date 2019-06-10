602c008ef564e8f46382b0d4edb293a7eb591143
Vasily93 <markovavasily@gmail.com>
2019-06-10 14:18:08 -0700

    rendered Inspiration component onto HomeApp

--- src/home/HomeApp.js
+++ src/home/HomeApp.js
@@ -1,6 +1,7 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
 import Footer from '../shared/Footer.js';
+import Inspiration from './Inspiration.js';
 
 class HomeApp extends Component {
     render() {
@@ -11,6 +12,9 @@ class HomeApp extends Component {
 
         const header = new Header();
         dom.insertBefore(header.render(), main);   
+
+        const inspiration = new Inspiration();
+        main.appendChild(inspiration.render());
         
         const footer = new Footer();
         footerTag.appendChild(footer.render());
---
+++ src/home/Inspiration.js
@@ -0,0 +1,14 @@
+import Component from '../Component.js';
+
+class Inspiration extends Component {
+    renderTemplate() {
+        return /*html*/`
+            <div class="advice-pic">
+                <h3>ADVICE IS HERE</h3>
+                <img src="">    
+            </div>
+        `;
+    }
+}
+
+export default Inspiration;
