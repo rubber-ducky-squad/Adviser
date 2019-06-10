60e7cf885ec17d5dce9485c761f1cf80899889f5
Vasily93 <markovavasily@gmail.com>
2019-06-10 11:00:44 -0700

    added Header renderTemplate and rendered whole header to home app

--- src/home/HomeApp.js
+++ src/home/HomeApp.js
@@ -1,6 +1,15 @@
 import Component from '../Component.js';
+import Header from '../shared/Header.js';
 
 class HomeApp extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const main = dom.querySelector('main');
+
+        const header = new Header();
+        dom.insertBefore(header.render(), main);        
+        return dom;
+    }
 
     renderTemplate() {
         return /*html*/ `
