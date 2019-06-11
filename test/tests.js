0f68497587a0d06721faafe38a76bea8b19ec72a
Vasily93 <markovavasily@gmail.com>
2019-06-10 17:49:02 -0700

    created a test in order to make testing suite work

--- test/tests.js
+++ test/tests.js
@@ -1,1 +1,8 @@
+import { app } from '../src/services/firebase.js';
 import './html-equal.js';
+// // individual test files...
+// import ‘./whatever.test.js’;
+import './Inspiration.test.js';
+QUnit.done(() => {
+    app.delete();
+});
