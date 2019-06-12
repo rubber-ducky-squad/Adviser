239f5326235495d21459c1edd73c66f2d5d9c2d3
DannySuarez <suarezd10@gmail.com>
2019-06-11 17:11:02 -0700

    we hooked up a modal for our list display, still need to put content inside, right now it pops up

--- src/lists/TaskItem.js
+++ src/lists/TaskItem.js
@@ -5,15 +5,22 @@ class TaskItem extends Component {
     render() {
         const dom = this.renderDOM();
         const removeButton = dom.querySelector('.delete-list');
+        const listButton = dom.querySelector('.list-button');
+        const modal = dom.querySelector('#modal');
         const list = this.props.list;
-        const key = auth.currentUser.uid;        
+        const key = auth.currentUser.uid;
 
+   
         const listRefs = listRef
             .child(key)
             .child(list.key);
  
         removeButton.addEventListener('click', () => {
             listRefs.remove();
+        });
+
+        listButton.addEventListener('click', () => {
+            modal.style.display = 'block';
         });
 
         return dom;
@@ -26,8 +33,15 @@ class TaskItem extends Component {
             <span>
                 <p>${list.title}</p>
                 <section>
-                    <button class="list-link"><a href="">go to list</a></button>
+                    <button class="list-button">go to list</button>
                     <button class="delete-list">X</button>
+                </section>
+                <section>
+                <div id="modal" class="modal">
+                    <div class="modal-content">
+                        <h1>hello</h1>
+                    </div>
+                </div>
                 </section>
             </span>
             </li>
