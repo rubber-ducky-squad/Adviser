eb9a3a79ce89f57fd027ea3cc17857414206258b
DannySuarez <suarezd10@gmail.com>
2019-06-12 10:51:33 -0700

    added cheeckbox into TodoItem html

--- src/lists/TodoItem.js
+++ src/lists/TodoItem.js
@@ -3,10 +3,13 @@ import Component from '../Component.js';
 class TodoItem extends Component {
     renderTemplate() {
         const todo = this.props.todo;
-        
+
         return /*html*/`
             <li>
-                <h1>${todo.todo}</h1>
+                <label for="${todo.todo}">
+                    <input name="checkbox" value="${todo.todo}" type="checkbox">${todo.todo}, complete by: ${todo.date}
+                    <button>X</button>
+                </label>
             </li>
         `;
     }
