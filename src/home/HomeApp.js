de870c422b39218a491b062f6609e923a871c97e
DannySuarez <suarezd10@gmail.com>
2019-06-12 16:14:55 -0700

    added a modal to our home app and styled it

--- src/home/HomeApp.js
+++ src/home/HomeApp.js
@@ -12,6 +12,12 @@ class HomeApp extends Component {
         const main = dom.querySelector('main');
         const footerTag = dom.querySelector('footer');
         const shuffleButton = dom.querySelector('button');
+        const todoButton = dom.querySelector('.todos');
+        const todoModal = dom.querySelector('#home-modal');
+
+        const todaysDate = Date();
+        const compareDate = todaysDate < 3;
+        console.log(compareDate)
 
         const header = new Header();
         dom.insertBefore(header.render(), main);   
@@ -21,6 +27,10 @@ class HomeApp extends Component {
 
         const inspiration = new Inspiration({ advice: [], pic: [] });
         main.appendChild(inspiration.render());
+
+        todoButton.addEventListener('click', () => {
+            todoModal.style.display = 'block';
+        });
         
         const footer = new Footer();
         footerTag.appendChild(footer.render());
@@ -60,6 +70,12 @@ class HomeApp extends Component {
             <div>
                 <main>
                 <button class="shuffle">SHUFFLE</button>
+                <button class="todos">todos for today</button>
+                <section>
+                <div id="home-modal" class="home-modal">
+                    <div class="home-modal-content">
+                        <button class="modal-exit">X</button>
+                    </div>
                 </main>
                 <footer>
                 </footer>
