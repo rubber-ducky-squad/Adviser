b56d43dcc4072c6a0385c971ee02fa71c8fcd350
DannySuarez <suarezd10@gmail.com>
2019-06-12 17:06:30 -0700

    filled out bare bones of HomeTodosItem

---
+++ src/home/HomeTodosItem.js
@@ -0,0 +1,10 @@
+import Component from '../Component.js';
+
+class HomeTodosItem extends Component {
+    renderTemplate() {
+        return /*html*/`
+            
+        `;
+    }
+}
+export default HomeTodosItem;
