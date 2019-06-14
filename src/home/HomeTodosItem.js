3a92f499bdc32cf1a4a88ffd3770a141e621e36c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-14 10:03:35 -0700

    created functionality to check a list item from the homepage and have it update within firebase and the todos page

--- src/home/HomeTodosItem.js
+++ src/home/HomeTodosItem.js
@@ -1,12 +1,33 @@
 import Component from '../Component.js';
+import { auth, todoRef } from '../services/firebase.js';
 
 class HomeTodosItem extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const todo = this.props.todo;
+    
+        const checkbox = dom.querySelector('#checkbox');
+
+        checkbox.addEventListener('change', () => {
+            todoRef.child(auth.currentUser.uid).child(todo.listKey).child(todo.key).update({
+                completed: checkbox.checked
+            });
+        });
+
+        return dom;
+    }
+
     renderTemplate() {
         const todo = this.props.todo;
+        let checked = '';
+        if(todo.completed) {
+            checked = 'checked';
+        }
         return /*html*/`
                 <section class="todays-todos">
-                    <label class="home-todo-align" for="${todo.todo}">
-                        <p>• ${todo.todo}</p>
+                    <label for="${todo.todo}">
+                        <input ${checked} id="checkbox" name="checkbox" value="${todo.completed}" type="checkbox">${todo.todo}
+                        <button class="todo-delete">X</button>
                     </label>
                 </section
             `;
